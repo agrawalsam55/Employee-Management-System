930212ca6b73d3406289617b92efc319ba493354
agrawalsam55 <135312926+agrawalsam55@users.noreply.github.com>
2025-05-02 01:14:29 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+@import "tailwindcss";
+
+
+* {
+  color: #fff;
+}  
+ html, body{
+  background-color: black;
+}
+ #tasklist::-webkit-scrollbar{
+  background-color: transparent;
+  display: none;
+ }
